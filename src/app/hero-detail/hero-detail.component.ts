d7569f2248fce25f303d9034095fcdadc9cc86a2
Raunak Shakya <rkshakya99@gmail.com>
2018-10-03 23:36:13 +0545

    complete angular-tour-of-heroes Part 8 - HTTP

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit, Input} from '@angular/core';
-import {Hero} from '../hero';
 import {ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
+
+import {Hero} from '../hero';
 import {HeroService} from '../hero.service';
 
 @Component({
@@ -10,7 +11,6 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-
   @Input() hero: Hero;
 
   constructor(
@@ -32,5 +32,10 @@ export class HeroDetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+
 import {Hero} from '../hero';
 import {HeroService} from '../hero.service';
 
@@ -8,25 +9,33 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
-  selectedHero: Hero;
 
-  // When Angular creates a HeroesComponent, the DI system sets the heroService parameter to the singleton instance of HeroService.
   constructor(private heroService: HeroService) {
   }
 
-  // The constructor shouldn't call a function that makes HTTP requests to a remote server. Instead, call getHeroes() inside the ngOnInit.
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
